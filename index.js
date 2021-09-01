e84c0c68d43a1ce503c3bc4155ae5ff440b608d9
sdefreese <samanthadefreese1@gmail.com>
2021-08-31 18:35:03 -0700

    commit tree b4a4ef8aafe514e55e6f2d9b00c3712caadbc3ce

--- index.js
+++ index.js
@@ -4,6 +4,27 @@
 
 
 function distanceFromHqInFeet(locationBlocks) {
-    return (distanceFromHqInBlocks() * 264)
+    return (distanceFromHqInBlocks(locationBlocks) * 264)
 }
-distanceFromHqInBlocks(43);
+
+function distanceTravelledInFeet(blockStart, blockEnd) {
+    return ((Math.abs(blockStart-blockEnd))* 264)
+}
+//let feet = (distanceTravelledInFeet(blockStart, blockEnd))
+
+function calculatesFarePrice(blockStart, blockEnd) {
+    if (distanceTravelledInFeet(blockStart, blockEnd) <=400) {
+        return 0
+    }
+    if (distanceTravelledInFeet(blockStart, blockEnd) <=2000) {
+        return (((distanceTravelledInFeet(blockStart, blockEnd))-400)*.02)
+    }
+    if (distanceTravelledInFeet(blockStart, blockEnd) <=2500) {
+        return 25
+    }
+    if (distanceTravelledInFeet(blockStart, blockEnd) >2500) {
+        return `cannot travel that far`
+    }
+}
+    
+        
